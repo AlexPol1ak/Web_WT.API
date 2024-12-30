e1a9977689adb364e8caea587e85a51f505f3270
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2024-12-30 04:07:45 +0300

    The database is connected. Created migrations and tables of categories and smartphones in the database. Excluded the git configuration file from tracking. Status: Lb 6.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Poliak_UI_WT.Domain.Entities;
+
+namespace Poliak_UI_WT.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Phone> Phones { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Poliak_UI_WT.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+string connectionString = builder.Configuration.GetConnectionString("MySQLConnectionLocal")
+    ?? throw new InvalidOperationException("Connection string 'Connection string' not found.");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
